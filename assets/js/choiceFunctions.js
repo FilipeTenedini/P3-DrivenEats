d291fa6cd8e1117ea59f2e53a4279b114d46fe7f
filipetenedini <tenedinifilipe@gmail.com>
2023-01-07 10:47:22 -0300

    add send msg function

--- assets/js/choiceFunctions.js
+++ assets/js/choiceFunctions.js
@@ -1,17 +1,16 @@
 import { allItems } from "./data/allItems.js";
 
-let cart = {
-    items: [],
-    prices:[]
-};
-let types = [];
+let cart = [];
+const types = [];
+let name;
+let local;
 
-
+// função de clique para selecionar e tirar seleção de cada item
 document.querySelectorAll('.option').forEach((item)=>{
     item.addEventListener('click', ()=>{
 
-        const itemPos = allItems.findIndex((i) => i.id == item.getAttribute('data-key'));
-        const type = allItems[itemPos].type;
+        const itemPosition = allItems.findIndex((i) => i.id == item.getAttribute('data-key'));
+        const type = allItems[itemPosition].type;
 
         if (types.includes(type)){
             const selectedItem = document.querySelector(`.${type} .selected`);
@@ -26,28 +25,87 @@ document.querySelectorAll('.option').forEach((item)=>{
         item.querySelector('ion-icon').classList.add('ion-active');
 
         if (document.querySelectorAll('.selected').length === 3){
-            updateCart();
             changeButton();
         }
 
     });
 })
 
+// trocando botão e viabilizando uso do mesmo
 function changeButton(){
-    const button = document.querySelector('.order');
+    const button = document.querySelector('.orderBtn');
     button.removeAttribute('disabled');
     button.classList.add('active');
     button.innerHTML = 'Fechar pedido';
 }
 
-
+// criando carrinho de compra e adicionando a compra;
 function updateCart(){
+    console.log(cart)
     document.querySelectorAll('.selected').forEach((item)=>{
-        const itemPos = allItems.findIndex((i) => i.id == item.getAttribute('data-key'));
-        const itemTitle = allItems[itemPos].title;
-        const itemPrice = allItems[itemPos].price;
+        const itemPosition = allItems.findIndex((i) => i.id == item.getAttribute('data-key'));
 
-        cart.items.push(itemTitle);
-        cart.prices.push(itemPrice);
+        allItems[itemPosition].qt = 1;
+        
+        cart.push(allItems[itemPosition])
     });
+
 }
+
+// capturando dados do usuário
+function captData() {
+    name = prompt('Qual seu nome?');
+    local = prompt('Qual o endereço?');
+}
+
+// inserindo dados no modal 
+function insertModalData() {
+    let total = 0;
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('invisible');
+    modal.classList.add('visible');
+
+    document.querySelectorAll('.order-items-item').forEach((item, index)=>{
+        total += cart[index].price;
+        item.querySelector('.order-item--name').innerHTML = cart[index].title
+        item.querySelector('.order-item--price').innerHTML = cart[index].price.toFixed(2);
+    });
+
+    modal.querySelector('.total-number').innerHTML = total.toFixed(2);
+}
+
+// inserindo função no primeiro button
+document.querySelector('.orderBtn').addEventListener('click', () => {
+    updateCart();
+    insertModalData();
+    captData();
+});
+
+// função de cancelar
+document.querySelector('.cancel').addEventListener('click', ()=> {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('visible');
+    modal.classList.add('invisible');
+    cart = [];
+})
+
+function sendMsg(){
+    let minhaString = encodeURIComponent(`
+Olá, gostaria de fazer o pedido:\n
+- Prato: ${document.querySelector('.dish .selected .option-title').innerHTML}
+- Bebida: ${document.querySelector('.drink .selected .option-title').innerHTML}
+- Sobremesa: ${document.querySelector('.dessert .selected .option-title').innerHTML}
+Total: R$ ${document.querySelector('.modal .total-number').innerHTML}
+\n
+Nome: ${name}
+Endereço: ${local}
+`)
+
+    let link = `https://wa.me/5551996693390?text=${minhaString}`;
+
+    document.querySelector('.modal a').setAttribute('href', link)
+}
+
+document.querySelector('.modal .confirm').addEventListener('click', ()=>{
+    sendMsg();
+})
